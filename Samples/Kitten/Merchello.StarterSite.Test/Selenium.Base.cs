7d678421d5bcf120874833b9232a28b3d7293bd2
Dina Berry <dinaberry@outlook.com>
2014-12-26 11:55:20 -0800

    beginning of the selenium tests

---
+++ Samples/Kitten/Merchello.StarterSite.Test/Selenium.Base.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Merchello.StarterSite.Test.Selenium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support;
+    using NUnit;
+    using NUnit.Framework;
+    using System.Diagnostics;
+    using System.IO;
+    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+
+    /// <summary>
+    /// http://stephenwalther.com/archive/2011/12/22/asp-net-mvc-selenium-iisexpress
+    /// </summary>
+    [TestFixture]
+    public abstract class SeleniumTest
+    {
+
+        const int iisPort = 59414;
+        private string _applicationName;
+        private Process _iisProcess;
+
+        public FirefoxDriver FirefoxDriver { get; set; }
+        //public ChromeDriver ChromeDriver { get; set; }
+        //public InternetExplorerDriver InternetExplorerDriver { get; set; }
+
+
+        protected SeleniumTest(string applicationName)
+        {
+            _applicationName = applicationName;
+        }
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Start IISExpress
+            StartIIS();
+
+            // Start Selenium drivers
+            this.FirefoxDriver = new FirefoxDriver();
+            //this.ChromeDriver = new ChromeDriver();
+            //this.InternetExplorerDriver = new InternetExplorerDriver();
+
+        }
+
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            // Ensure IISExpress is stopped
+            if (_iisProcess.HasExited == false)
+            {
+                _iisProcess.Kill();
+            }
+
+
+            // Stop all Selenium drivers
+            this.FirefoxDriver.Quit();
+            //this.ChromeDriver.Quit();
+            //this.InternetExplorerDriver.Quit();
+
+        }
+
+
+
+        private void StartIIS()
+        {
+            var applicationPath = GetApplicationPath(_applicationName);
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+            _iisProcess = new Process();
+            _iisProcess.StartInfo.FileName = programFiles + "\\IIS Express\\iisexpress.exe";
+            _iisProcess.StartInfo.Arguments = string.Format("/path:" + "{0}" + " /port:{1}", applicationPath, iisPort);
+            _iisProcess.Start();
+        }
+
+
+        protected virtual string GetApplicationPath(string applicationName)
+        {
+            var solutionFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+            return Path.Combine(solutionFolder, applicationName);
+        }
+
+
+        public string GetAbsoluteUrl(string relativeUrl)
+        {
+            if (!relativeUrl.StartsWith("/"))
+            {
+                relativeUrl = "/" + relativeUrl;
+            }
+            return String.Format("http://localhost:{0}{1}", iisPort, relativeUrl);
+        }
+
+
+    }
+}
---
+++ Samples/Kitten/Merchello.StarterSite.Test/Selenium.HomePage.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Merchello.StarterSite.Test.Selenium
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support;
+    using NUnit;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Home page test for kitten site test
+    /// Merchello features only. 
+    /// </summary>
+    [TestFixture]
+    public class HomePageTests : SeleniumTest
+    {
+ 
+        public HomePageTests() : base("Merchello.Kitten") { }
+ 
+ 
+        [Test]
+        public void Did_quicklinks_display () {
+
+            // xpath to quicklink div
+            var xpathToQuickLinks = "//*[@id=\'quick-links\']";
+            
+            // Act
+            this.FirefoxDriver.Navigate().GoToUrl(this.GetAbsoluteUrl("/"));
+            
+            // Assert
+            Assert.IsTrue(this.FirefoxDriver.FindElement(By.XPath(xpathToQuickLinks)).Displayed);
+        }
+    }
+}
