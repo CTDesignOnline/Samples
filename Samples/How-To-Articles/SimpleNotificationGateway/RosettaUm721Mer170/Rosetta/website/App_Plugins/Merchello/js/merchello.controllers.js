2f274483edccb6b6a00303d3aaeeaba397c8bd37
Dina Berry <dinaberry@outlook.com>
2015-03-09 13:30:13 -0700

    notification provider updated to 1.8

--- Samples/How-To-Articles/SimpleNotificationGateway/RosettaUm721Mer170/Rosetta/website/App_Plugins/Merchello/js/merchello.controllers.js
+++ Samples/How-To-Articles/SimpleNotificationGateway/RosettaUm721Mer170/Rosetta/website/App_Plugins/Merchello/js/merchello.controllers.js
@@ -777,6 +777,14 @@
 
         }]);
 
+angular.module('merchello').controller('MerchelloDashboardController',
+    ['assetsService',
+    function(assetsService) {
+
+        assetsService.loadCss('/App_Plugins/Merchello/assets/merchello.css');
+
+    }]);
+
 /**
  * @ngdoc controller
  * @name Merchello.Backoffice.SettingsController
@@ -869,13 +877,10 @@ angular.module('merchello').controller('Merchello.Backoffice.SettingsController'
             // exposed methods
             $scope.save = save;
 
-            console.info($scope.dialogData);
-
             function save() {
                 $scope.dialogData.notificationMessage.monitorKey = $scope.dialogData.selectedMonitor.monitorKey;
                 $scope.submit($scope.dialogData);
             }
-
     }]);
 
 angular.module('merchello').controller('Merchello.GatewayProviders.Dialogs.NotificationsMethodAddEditController',
@@ -991,10 +996,12 @@ angular.module('merchello')
             $scope.rateTable = {}; //shipFixedRateTableDisplayBuilder.createDefault();
             $scope.rateTable.shipMethodKey = ''; //$scope.dialogData.method.key;
 
+
             // exposed methods
             $scope.addRateTier = addRateTier;
             $scope.insertRateTier = insertRateTier;
             $scope.cancelRateTier = cancelRateTier;
+            $scope.removeRateTier = removeRateTier;
             $scope.save = save;
 
             /**
@@ -1171,10 +1178,10 @@ angular.module('merchello').controller('Merchello.GatewayProviders.Dialogs.Wareh
     }]);
 
 angular.module('merchello').controller('Merchello.Directives.ShipCountryGatewaysProviderDirectiveController',
-    ['$scope', 'notificationsService', 'dialogService',
+    ['$scope', 'notificationsService', 'dialogService', 'settingsResource',
         'shippingGatewayProviderResource', 'shippingGatewayProviderDisplayBuilder', 'shipMethodDisplayBuilder',
         'shippingGatewayMethodDisplayBuilder', 'gatewayResourceDisplayBuilder', 'dialogDataFactory',
-        function($scope, notificationsService, dialogService,
+        function($scope, notificationsService, dialogService, settingsResource,
                  shippingGatewayProviderResource, shippingGatewayProviderDisplayBuilder, shipMethodDisplayBuilder,
                  shippingGatewayMethodDisplayBuilder, gatewayResourceDisplayBuilder, dialogDataFactory) {
 
@@ -1182,7 +1189,7 @@ angular.module('merchello').controller('Merchello.Directives.ShipCountryGateways
             $scope.allProviders = [];
             $scope.assignedProviders = [];
             $scope.availableProviders = [];
-
+            $scope.currencySymbol = '';
 
             // exposed methods
             $scope.deleteCountry = deleteCountry;
@@ -1201,7 +1208,25 @@ angular.module('merchello').controller('Merchello.Directives.ShipCountryGateways
              * Initializes the controller
              */
             function init() {
+                loadSettings();
                 loadCountryProviders();
+            }
+
+            /**
+             * @ngdoc method
+             * @name init
+             * @function
+             *
+             * @description
+             * Loads the currency settings
+             */
+            function loadSettings() {
+                var currencySymbolPromise = settingsResource.getCurrencySymbol();
+                currencySymbolPromise.then(function(currencySymbol) {
+                    $scope.currencySymbol = currencySymbol;
+                }, function (reason) {
+                    notificationsService.error("Settings Load Failed", reason.message);
+                });
             }
 
             /**
@@ -1326,6 +1351,7 @@ angular.module('merchello').controller('Merchello.Directives.ShipCountryGateways
             function editShippingMethodRegionsOpen(gatewayMethod) {
                 var dialogData = dialogDataFactory.createEditShippingGatewayMethodDialogData();
                 dialogData.shippingGatewayMethod = gatewayMethod;
+                dialogData.currencySymbol = $scope.currencySymbol;
                 dialogService.open({
                     template: '/App_Plugins/Merchello/Backoffice/Merchello/Dialogs/shipping.shipmethod.regions.html',
                     show: true,
@@ -1410,6 +1436,7 @@ angular.module('merchello').controller('Merchello.Directives.ShipCountryGateways
             function editShippingMethodDialogOpen(gatewayMethod) {
                 var dialogData = dialogDataFactory.createEditShippingGatewayMethodDialogData();
                 dialogData.shippingGatewayMethod = gatewayMethod;
+                dialogData.currencySymbol = $scope.currencySymbol;
                 var editor = gatewayMethod.dialogEditorView.editorView;
                 dialogService.open({
                     template: editor,
@@ -1664,9 +1691,11 @@ angular.module("umbraco").controller("Merchello.Backoffice.GatewayProvidersListC
              * Handles the data passed back from the provider editor dialog and saves it to the database
              */
             function providerConfigDialogConfirm(data) {
+                $scope.preValuesLoaded = false;
                 var promise = gatewayProviderResource.saveGatewayProvider(data.provider);
                 promise.then(function (provider) {
                         notificationsService.success("Gateway Provider Saved", "");
+                        init();
                     },
                     function (reason) {
                         notificationsService.error("Gateway Provider Save Failed", reason.message);
@@ -5512,7 +5541,7 @@ angular.module('merchello').controller('Merchello.Backoffice.OrderShipmentsContr
                 var found = false;
                 while(i < $scope.invoice.orders.length && !found) {
                     var item = _.find($scope.invoice.orders[ i ].items, function(item) {
-                      return item.shipmentKey === '' || item.shipmentKey === null;
+                      return (item.shipmentKey === '' || item.shipmentKey === null) && item.extendedData.getValue('merchShippable').toLowerCase() === 'true';
                     });
                     if(item !== null && item !== undefined) {
                         found = true;
